2ca984c862a160cfd49f5a868e82f9180c25dfe2
Gabriel Mijares <gabrielmljob@gmail.com>
2020-02-26 19:50:44 +0100

    test: add jest test for exercise #9/21 missing letters

---
+++ missingLetter.test.js
@@ -0,0 +1,12 @@
+const fearNotLetter = require('./index');
+
+describe('fearNotLetter function', () => {
+	test('it should return the missing letter', () => {
+		const input = "abce";
+		const output = "x";
+
+		expect(fearNotLetter(input)).toEqual(output);
+	});
+});
+
+
