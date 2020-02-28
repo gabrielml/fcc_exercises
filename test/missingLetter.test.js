4c5009e87159a545ba829c82540db46d62290384
Gabriel Mijares <gabrielmljob@gmail.com>
2020-02-28 10:09:43 +0100

    test: fix and refactor missingLetter test

--- test/missingLetter.test.js
+++ test/missingLetter.test.js
@@ -1,12 +1,10 @@
-const fearNotLetter = require('./index');
+const fearNotLetter = require('../index');
 
 describe('fearNotLetter function', () => {
 	test('it should return the missing letter', () => {
 		const input = "abce";
-		const output = "x";
+		const output = "d";
 
 		expect(fearNotLetter(input)).toEqual(output);
 	});
 });
-
-
